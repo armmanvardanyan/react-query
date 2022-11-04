ca09f6c8ca2f63dd099840655744f5d183dd56d5
Arman <av3838@mail.ru>
2022-11-05 01:21:12 +0400

    refetchOnMount refetchOnWindowFocus

--- src/components/RqSuperheroes.page.js
+++ src/components/RqSuperheroes.page.js
@@ -10,8 +10,10 @@ const fetchSuperHeroes = () => {
 export const RqSuperHero = () => {
 
     const {data,isLoading,isError,error, isFetching, refetch} = useQuery("super-heroes", fetchSuperHeroes,{
-     //   cacheTime: 5000, // this says cache data during 5 seconds then data will be garbage collected default 30000 (5 minutes)
-        staleTime: 30000 // this says that there is no need to refetch data during 5 minutes seconds default 0
+       //cacheTime: 5000, // this says cache data during 5 seconds then data will be garbage collected default 30000 (5 minutes)
+       // staleTime: 30000, // this says that there is no need to refetch data during 5 minutes seconds default 0
+        refetchOnMount: false, //dont refetch data if component rerendered
+        refetchOnWindowFocus: false
     });
 
 
