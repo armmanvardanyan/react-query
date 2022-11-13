8acbb6453009884f833d9eb992ff31763134db72
Arman <av3838@mail.ru>
2022-11-13 23:02:17 +0400

    Initial Query Data (user cached data)

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery , useQueryClient } from "react-query";
 
 
 
@@ -10,7 +10,21 @@ const fetchSuperHero = ({queryKey}) => {
 
 export const UseSuperHeroData = (heroId) => {
 
-    return useQuery(['super-hero',heroId],fetchSuperHero)
+    const queryClient = useQueryClient();
+
+    return useQuery(['super-hero',heroId],fetchSuperHero,{
+        initialData: () => {
+            const hero = queryClient.getQueryData("super-heroes")?.data?.find(hero => hero.id === parseInt(heroId))
+            if(hero) {
+                return {
+                    data: hero
+                }
+            }else {
+                // Otherwise, return undefined and let it fetch from a hard loading state!
+                return undefined;
+            }
+        } 
+    })
 
     
 }
